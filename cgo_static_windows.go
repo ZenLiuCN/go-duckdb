ac472b1753e768ec9aa7ad62cff9cb5ef7cbb944
Zen.Liu <lcz20@163.com>
2023-12-25 18:52:42 +0800

    add auto extract extensions (those are only supported extensions)

--- cgo_static_windows.go
+++ cgo_static_windows.go
@@ -17,3 +17,45 @@ package duckdb
 #include <duckdb.h>
 */
 import "C"
+import (
+	_ "embed"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+var (
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/postgres_scanner.duckdb_extension
+	pgs []byte
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/icu.duckdb_extension
+	icu []byte
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/inet.duckdb_extension
+	inet []byte
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/sqlsmith.duckdb_extension
+	sqlsmith []byte
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/tpcds.duckdb_extension
+	tpcds []byte
+	//go:embed deps/windows_amd64_extensions/.duckdb/extensions/v0.9.2/windows_amd64/visualizer.duckdb_extension
+	visualizer []byte
+)
+
+func init() {
+	ud, err := os.UserHomeDir()
+	if err != nil {
+		return
+	}
+	ext := filepath.Join(ud, ".duckdb", "extensions", "v0.9.2", "windows_amd64")
+	for k, b := range map[string][]byte{
+		"postgres_scanner.duckdb_extension": pgs,
+		"sqlsmith.duckdb_extension":         sqlsmith,
+		"tpcds.duckdb_extension":            tpcds,
+		"visualizer.duckdb_extension":       visualizer,
+		"icu.duckdb_extension":              icu,
+		"inet.duckdb_extension":             inet,
+	} {
+		p := filepath.Join(ext, k)
+		if _, err = os.Stat(p); err != nil && errors.Is(err, os.ErrNotExist) {
+			_ = os.WriteFile(p, b, os.ModePerm)
+		}
+	}
+}
